65304dd66424292362a842a45a26fcd2ea999a22
K1ra-Python <makismka.guchenko@mail.ru>
2023-06-30 03:28:56 +0300

    #

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="text-center">
+    </div>
+    <h1 class="text-center">Nuxt PreInstall</h1>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Home Page - Nuxt Preinstall app",
+});
+</script>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+
+.more-buttons {
+  display: flex;
+  justify-content: center;
+}
+
+.learn-more-button {
+  margin-right: 1rem;
+}
+</style>
